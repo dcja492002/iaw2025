fd42b5f75cabcad14c8feea7be8d1e9bd1b5c4ea
dcja492002 <dcja492002@gmail.com>
2025-02-24 13:58:03 +0100

    variables

---
+++ variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $numero = 42;
+        $texto = "Hola, mundo";
+        $arreglo = [1, 2, 3];
+
+        var_dump($numero);
+        echo "<br>";
+        var_dump($texto);
+        echo "<br>";
+        var_dump($arreglo);
+        ?>
+
+</body>
+</html>
